a76a1084b66fdcb68a3907a3ffabdf887516eb19
AlbertaBeef <grouby177@gmail.com>
2021-03-23 14:41:24 +0000

    Added FPS display and power plot.

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,Response
+from flask import Flask,render_template,request,make_response,Response
 import numpy as np
 import cv2
 import base64
@@ -8,8 +8,11 @@ import os
 
 global vart_present
 global dlib_present
+global plot_present
 vart_present = False
 dlib_present = False
+dlib_present = False
+plot_present = False
 
 try:
    import vart
@@ -17,13 +20,25 @@ try:
    import xir
    vart_present = True
 except:
-   print("[INFO] VART not available ! (are you running on a Xilinx platform ?)")
+   print("[ERROR] VART not available ! (are you running on a Vitis-AI 1.3 platform ?)")
    
 try:
    import dlib
    dlib_present = True
 except:
-   print("[INFO] DLIB not available ! (install with : pip3 install dlib)")
+   print("[ERROR] DLIB not available ! (install with : pip3 install dlib)")
+
+
+try:
+   import sensors
+   import time
+   import matplotlib.pyplot as plt
+   from matplotlib.figure import Figure
+   from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+   import io
+   plot_present = True
+except:
+   print("[ERROR] sensor/matplotlib/io not available ! (install with : pip3 install pysensors matplotlib io)")
 
 
 if vart_present == True:
@@ -32,6 +47,8 @@ if vart_present == True:
    from vitis_ai_vart.facedetect import FaceDetect
    from vitis_ai_vart.facelandmark import FaceLandmark
    from vitis_ai_vart.utils import get_child_subgraph_dpu
+
+
 
 # Define App
 app = Flask(__name__,template_folder="templates")
@@ -53,58 +70,18 @@ def index():
 global detThreshold
 global nmsThreshold
 global ai_algorithm
+global display_fps
 detThreshold = 0.55
 nmsThreshold = 0.35
 #ai_algorithm = 0 # none
 ai_algorithm = 1 # face detection
+display_fps = 0
 
 # VART versus DLIB algorithm selection
 global use_dlib_detection
 global use_dlib_landmarks
 use_dlib_detection = False
 use_dlib_landmarks = False
-
-# Handles uploaded image
-@app.route('/upload',methods=["POST"])
-def upload():
-  # Get uploaded form
-  d = request.form
-  # Extract the data field
-  data = d.get('data')
-  
-  # The first part of the string simply indicates 
-  # what kind of file it is. So we extract only the data part. 
-  data = data.split(',')[1]
-
-  # Get base64 decoded 
-  data = base64.decodebytes(data.encode())
-  
-  # Convert to numpy array
-  nparr = np.frombuffer(data, np.uint8)
-
-  # Read image
-  img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-  cv2.imwrite("/tmp/test.jpg", img)
-  
-  # Create a 4D blob from image
-  blob = cv2.dnn.blobFromImage(img, 1/255, (28, 28))
-
-  # Run a model
-  #net.setInput(blob)
-  #out = net.forward()
-  
-  # Get a class with a highest score
-  #out = softmax(out.flatten())
-  #classId = np.argmax(out)
-  #confidence = out[classId]
-  classId = 1
-  confidence = 0.00
-
-  # Print results on the server side
-  print("classId: {} confidence: {}".format(classId, confidence), file=sys.stdout)
-  
-  # Return result as a json object
-  return json.dumps({'success':True, 'class': int(classId), 'confidence': float(confidence)}), 200, {'ContentType':'application/json'} 
 
 @app.route('/set_threshold/<slider>/<value>',methods=["POST"])
 def set_threshold(slider,value):
@@ -169,6 +146,20 @@ def set_dlib_option(algo,value):
    # Return result as a json object
    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
+@app.route('/set_fps_option/<value>',methods=["POST"])
+def set_fps_option(value):
+   global display_fps
+
+   if value == "true": 
+      display_fps = True
+      print("[INFO] display FPS = ON")
+   if value == "false": 
+      display_fps = False
+      print("[INFO] display FPS = OFF")
+
+   # Return result as a json object
+   return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
 
 def generate():
    global vart_present
@@ -176,6 +167,7 @@ def generate():
    global detThreshold
    global nmsThreshold
    global ai_algorithm
+   global display_fps
    global use_dlib_detection
    global use_dlib_landmarks
 
@@ -247,7 +239,20 @@ def generate():
                          )
    print("[INFO] Camera Matrix :\n {0}".format(camera_matrix));
 
+   # init the real-time FPS display
+   rt_fps_count = 0;
+   rt_fps_time = cv2.getTickCount()
+   rt_fps_valid = False
+   rt_fps = 0.0
+   rt_fps_message = "FPS: {0:.2f}".format(rt_fps)
+   rt_fps_x = 10
+   rt_fps_y = size[0]-10
+
    while True:
+      # Update the real-time FPS counter
+      if rt_fps_count == 0:
+         rt_fps_time = cv2.getTickCount()
+
       # if closed by someone else
       #if not cap.isOpened():
       #   print("[INFO] WEBCAM (/dev/video0) released by someone else.")
@@ -384,6 +389,10 @@ def generate():
                p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
                cv2.line(frame, p1, p2, (255,0,0), 2)
 
+      # real-time FPS display
+      if display_fps == True and rt_fps_valid == True:
+         cv2.putText(frame, rt_fps_message, (rt_fps_x,rt_fps_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+
       # Encode video frame to JPG
       (flag, encodedImage) = cv2.imencode(".jpg", frame)
       if not flag:
@@ -392,11 +401,176 @@ def generate():
       # yield the output frame in the byte format
       yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 
+      # Update the real-time FPS counter
+      rt_fps_count = rt_fps_count + 1
+      if rt_fps_count >= 10:
+         t = (cv2.getTickCount() - rt_fps_time)/cv2.getTickFrequency()
+         rt_fps_valid = True
+         rt_fps = 10.0/t
+         rt_fps_message = "FPS: {0:.2f}".format(rt_fps)
+         #print("[INFO] ",rt_fps_message)
+         rt_fps_count = 0
+
 @app.route("/video_feed")
 def video_feed():
 	# return the response generated along with the specific media
 	# type (mime type)
    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+def monitor():
+
+  if plot_present == True:
+   sensors.init()
+   
+   pmbus_ultra96v2 = {
+      # ir38060-i2c-6-45
+      "ir38060-i2c-6-45:pout1" : "5V",
+      # irps5401-i2c-6-43
+      "irps5401-i2c-6-43:pout1" : "VCCAUX",
+      "irps5401-i2c-6-43:pout2" : "VCCO 1.2V",
+      "irps5401-i2c-6-43:pout3" : "VCCO 1.1V",
+      "irps5401-i2c-6-43:pout4" : "VCCINT",
+      "irps5401-i2c-6-43:pout5" : "3.3V DP",
+      # irps5401-i2c-6-44
+      "irps5401-i2c-6-44:pout1" : "VCCPSAUX",
+      "irps5401-i2c-6-44:pout2" : "PSINT_LP",
+      "irps5401-i2c-6-44:pout3" : "VCCO 3.3V",
+      "irps5401-i2c-6-44:pout4" : "PSINT_FP",
+      "irps5401-i2c-6-44:pout5" : "PSPLL 1.2V",
+   }
+
+   pmbus_uz7ev_evcc = {
+      # ir38063-i2c-3-4c
+      "ir38063-i2c-3-4c:pout1" : "Carrier 3V3",
+      # ir38063-i2c-3-4b
+      "ir38063-i2c-3-4b:pout1" : "Carrier 1V8",
+      # irps5401-i2c-3-4a
+      "irps5401-i2c-3-4a:pout1" : "Carrier 0V9 MGTAVCC",
+      "irps5401-i2c-3-4a:pout2" : "Carrier 1V2 MGTAVTT",
+      "irps5401-i2c-3-4a:pout3" : "Carrier 1V1 HDMI",
+      "irps5401-i2c-3-4a:pout4" : "Unused",
+      "irps5401-i2c-3-4a:pout5" : "Carrier 1V8 MGTVCCAUX LDO",
+      # irps5401-i2c-3-49
+      "irps5401-i2c-3-49:pout1" : "Carrier 0V85 MGTRAVCC",
+      "irps5401-i2c-3-49:pout2" : "Carrier 1V8 VCCO",
+      "irps5401-i2c-3-49:pout3" : "Carrier 3V3 VCCO",
+      "irps5401-i2c-3-49:pout4" : "Carrier 5V MAIN",
+      "irps5401-i2c-3-49:pout5" : "Carrier 1V8 MGTRAVTT LDO",
+      # ir38063-i2c-3-48
+      "ir38063-i2c-3-48:pout1" : "SOM 0V85 VCCINT",
+      # irps5401-i2c-3-47
+      "irps5401-i2c-3-47:pout1" : "SOM 1V8 VCCAUX",
+      "irps5401-i2c-3-47:pout2" : "SOM 3V3",
+      "irps5401-i2c-3-47:pout3" : "SOM 0V9 VCUINT",
+      "irps5401-i2c-3-47:pout4" : "SOM 1V2 VCCO_HP_66",
+      "irps5401-i2c-3-47:pout5" : "SOM 1V8 PSDDR_PLL LDO",
+      # irps5401-i2c-3-46
+      "irps5401-i2c-3-46:pout1" : "SOM 1V2 VCCO_PSIO",
+      "irps5401-i2c-3-46:pout2" : "SOM 0V85 VCC_PSINTLP",
+      "irps5401-i2c-3-46:pout3" : "SOM 1V2 VCCO_PSDDR4_504",
+      "irps5401-i2c-3-46:pout4" : "SOM 0V85 VCC_PSINTFP",
+      "irps5401-i2c-3-46:pout5" : "SOM 1V2 VCC_PSPLL LDO",
+   }
+
+   pmbus_uz3eg_xxx = {
+      # irps5401-i2c-3-43
+      "irps5401-i2c-3-43:pout1" : "PSIO",
+      "irps5401-i2c-3-43:pout2" : "VCCAUX",
+      "irps5401-i2c-3-43:pout3" : "PSINTLP",
+      "irps5401-i2c-3-43:pout4" : "PSINTFP",
+      "irps5401-i2c-3-43:pout5" : "PSPLL",
+      # irps5401-i2c-3-44
+      "irps5401-i2c-3-44:pout1" : "PSDDR4",
+      "irps5401-i2c-3-44:pout2" : "INT_IO",
+      "irps5401-i2c-3-44:pout3" : "3.3V",
+      "irps5401-i2c-3-44:pout4" : "INT",
+      "irps5401-i2c-3-44:pout5" : "PSDDRPLL",
+      # irps5401-i2c-3-45
+      "irps5401-i2c-3-45:pout1" : "MGTAVCC",
+      "irps5401-i2c-3-45:pout2" : "5V",
+      "irps5401-i2c-3-45:pout3" : "3.3V",
+      "irps5401-i2c-3-45:pout4" : "VCCO 1.8V",
+      "irps5401-i2c-3-45:pout5" : "MGTAVTT",
+   }
+
+   pmbus_annotations = {
+      "ULTRA96V2"   : pmbus_ultra96v2,
+      "UZ7EV_EVCC"  : pmbus_uz7ev_evcc,
+      "UZ3EG_IOCC"  : pmbus_uz3eg_xxx,
+      "UZ3EG_PCIEC" : pmbus_uz3eg_xxx
+   }
+
+   use_annotations = 1
+   target = "ULTRA96V2"
+   target_annotations = pmbus_annotations[target]
+
+   pmbus_power_features = []
+   for chip in sensors.iter_detected_chips():
+      device_name = str(chip)
+      adapter_name = str(chip.adapter_name)
+      print( "%s at %s" % (device_name, adapter_name) )
+      #if 'irps5401' in device_name:
+      #if 'ir38063' in device_name:
+      if True:
+         for feature in chip:
+            feature_name = str(feature.label)
+            if 'pout' in feature_name:
+               label = device_name + ":" + feature_name
+               pmbus_power_features.append( (label, feature) )
+               feature_value = feature.get_value() * 1000.0
+               print( " %s : %8.3f mW" % (feature_name, feature_value) )
+
+   N = len(pmbus_power_features)
+   p_x = np.linspace(1,N,N)
+   p_y = np.linspace(0,0,N,dtype=float)
+
+   p_l = []
+   for (i, (label,feature)) in enumerate(pmbus_power_features):
+      p_l.append(label)
+   if use_annotations == 1:
+      for i in range( len(p_l) ):
+         label = p_l[i]
+         if label in target_annotations:
+            label = target_annotations[label]
+            p_l[i] = label
+
+
+   fig = plt.figure()
+   a = fig.add_subplot(1,1,1)
+   a.barh(p_x,p_y) 
+
+   while True:
+      for (i, (label,feature)) in enumerate(pmbus_power_features):
+         p_y[i] = feature.get_value() * 1000.0
+
+      fig.delaxes(a)
+      a = fig.add_subplot(1,1,1)
+      a.barh(p_x,p_y)
+      a.set_xlabel('Power (mW)')
+      a.set_xlim(xmin=0.0,xmax=10000.0)
+      a.set_yticks(p_x)
+      a.set_yticklabels(p_l)
+
+      fig.canvas.draw()
+
+      plot_img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+      plot_img = plot_img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+      plot_img = cv2.cvtColor(plot_img,cv2.COLOR_RGB2BGR)
+
+      # Encode video frame to JPG
+      (plot_flag, plot_encodedImage) = cv2.imencode(".jpg", plot_img)
+      if not plot_flag:
+         continue
+      
+      # yield the output frame in the byte format
+      yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(plot_encodedImage) + b'\r\n')
+
+@app.route("/power_feed")
+def power_feed():
+   # return the response generated along with the specific media
+   # type (mime type)
+   return Response(monitor(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == '__main__':
    #app.run(debug = True)
